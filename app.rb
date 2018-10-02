63b1057d352b0adb7621d4c35e909646dce241b5
Brent Bauman <brentbauman86@gmail.com>
2018-10-02 16:07:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,42 +1,36 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
   get '/reversename/:name' do
-    @revname = params[:name].reverse
-    "#{@revname}"
+    @name = params[:name]
+    erb :reversename
   end
 
   get '/square/:number' do
-    @square = params[:number].to_i
-    "#{@square ** 2}"
-    # binding.pry
+    @num = params[:number].to_i
+    erb :square
   end
 
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    "#{@phrase * @number}"
-    # binding.pry
+    erb :say
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word1 = params[:word1]
-    @word2 = params[:word2]
-    @word3 = params[:word3]
-    @word4 = params[:word4]
-    @word5 = params[:word5]
-    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
-    # binding.pry
+    @w1 = params[:word1]
+    @w2 = params[:word2]
+    @w3 = params[:word3]
+    @w4 = params[:word4]
+    @w5 = params[:word5]
+    erb :saywords
   end
 
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
-    "#{@num1 + @num2
-      @num2 - @num1
-      @num1 * @num2
-      @num1 / @num2}"
+    erb :operation
   end
+
 end
